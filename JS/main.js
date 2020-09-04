ddedf3bada32250c2103f2de238360bfbd5628cc
suryanarayanan21 <suryanarayanan.kviisc@gmail.com>
2020-09-04 12:53:43 +0530

    Added dependencies using npm

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,4 @@
-import * as THREE from "./three.js-master/build/three.module.js";
+import * as THREE from "../node_modules/three/build/three.module.js";
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
